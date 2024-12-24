6d50ca47b93201555bfaba0b0d9954ac8c43e0a8
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-23 23:03:37 -0300

    variavei

---
+++ JAVASCRIPT/variaveis/DOCS/JS/script.js
@@ -0,0 +1,24 @@
+//String
+var texto;
+
+texto = 'OI, tudo bem ?';
+alert(texto);
+
+//Number
+
+var numer_inteiro = -5;
+var numero_fracionario = 4.65;
+alert(numer_inteiro);
+alert( numero_fracionario);
+alert(numer_inteiro + numero_fracionario);
+
+//Boolean
+
+var certo = true;
+
+document.write(texto);
+
+console.log(texto);
+
+
+
