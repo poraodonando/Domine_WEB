4fd00c6e6e4d0506cfa43267e6fb95f212d340af
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-25 18:43:22 -0300

    adicioando funções

---
+++ JAVASCRIPT/Funcoes/script.js
@@ -0,0 +1,11 @@
+
+function calcularArea(base,altura){
+   var area = base*altura;
+
+    return area;
+}
+
+//var calc_area = calcularArea();
+
+document.write("A area possui "+calcularArea(4,533)+" m2");
+
