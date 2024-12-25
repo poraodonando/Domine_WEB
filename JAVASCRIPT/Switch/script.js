c4a379894f8895ca765961ecb73ea9a1444beb32
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-25 11:31:50 -0300

    adicionando o switch

---
+++ JAVASCRIPT/Switch/script.js
@@ -0,0 +1,29 @@
+
+var numero = parseInt(prompt("Digite um número: "));
+
+
+switch (numero){
+    
+    case 1:
+        document.write("Escolheu 1");
+        break;
+    
+    case 2:
+        document.write("Escolheu 2");
+        break;
+    
+    case 3:
+        document.write("Escolheu 3");
+        break;
+    
+    case 4:
+        document.write("Escolheu o 4");
+        break;
+    
+    default:
+        document.write("Escolheu um numero qualquer")
+
+
+
+
+}
