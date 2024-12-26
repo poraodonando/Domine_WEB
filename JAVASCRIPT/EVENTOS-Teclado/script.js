0a87e10440878c8a89e389926c3a097464cee6d1
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-26 16:07:26 -0300

    eventos - teclado

---
+++ JAVASCRIPT/EVENTOS-Teclado/script.js
@@ -0,0 +1,12 @@
+
+function on_keydown(){
+    alert("está funcionando")
+}
+
+function on_keyup(){
+    alert("key up está funcionando")
+}
+
+function on_keypress(){
+    alert("Keypress funcionado")
+}
