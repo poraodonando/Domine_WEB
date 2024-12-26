050540e2f1b355e891c96a6dd086790a4814af40
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-26 15:56:14 -0300

    eventos do mouse

---
+++ JAVASCRIPT/EVENTOS-Mouse/script.js
@@ -0,0 +1,22 @@
+
+
+function acao_click(){
+    alert("Evento disparar")
+}
+
+function acao_b_click(){
+    algo = prompt("Digite algo")
+
+}
+
+function on_mouseup(){
+    alert("On mouse up")
+}
+
+function on_mouseover(){
+    alert("On mouuse over")
+}
+
+function on_mouseout(){
+    alert("Mouse out")
+}
