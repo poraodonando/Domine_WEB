d2d2bd6767869a731e2818bf97bec5fd77bb91d0
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-26 20:37:26 -0300

    dom 3

---
+++ JAVASCRIPT/DOM-III/script.js
@@ -0,0 +1,8 @@
+function modifica_estilo(cor, largura, altura){
+    
+    document.getElementById('bloco').style.background = cor;
+   
+    document.getElementById('bloco').style.width = largura
+
+    document.getElementById('bloco').style.width = altura
+}
